3cd80af30085ce4a80736698074935db27295eea
Lucas Lima Taffo <lucas.taffo@gmail.com>
2021-08-05 15:44:07 -0400

    finished styling for today I think

--- client/Signup.jsx
+++ client/Signup.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
-import auth from "./Auth";
+import React from 'react';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import auth from './Auth';
+import { Link } from 'react-router-dom';
 
 const Signup = (props) => {
-  const [userData, setUserData] = useState({ username: "", password: "" });
+  const [userData, setUserData] = useState({ username: '', password: '' });
   const history = useHistory();
 
   const handleUsernameInputChange = (e) => {
@@ -26,10 +27,10 @@ const Signup = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(userData);
-    fetch("/api/users/signup", {
-      method: "POST",
+    fetch('/api/users/signup', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username: userData.username,
@@ -38,39 +39,50 @@ const Signup = (props) => {
     })
       .then((response) => {
         console.log(
-          "we have successfully made a post request with the user data",
+          'we have successfully made a post request with the user data',
           response
         );
-        history.push("/");
+        history.push('/');
       })
       .catch((err) => {
-        console.log("Error making fetch request", err);
+        console.log('Error making fetch request', err);
       });
-    history.push("/");
+    history.push('/');
   };
 
   return (
     <div>
-      <h1>Welcome, please sign up.</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          id="username"
-          className="form-field"
-          type="text"
-          name="username"
-          value={userData.username}
-          onChange={handleUsernameInputChange}
-        />
-        <input
-          id="username"
-          className="form-field"
-          type="text"
-          name="username"
-          value={userData.password}
-          onChange={handlePasswordInputChange}
-        />
-        <input type="submit" value="submit" />
-      </form>
+      <div className='top-menu-container'>
+        <Link to={'/'} className='brandname'>
+          Shy Panda
+        </Link>
+      </div>
+      <div className='signup'>
+          <div className="signupContent">
+        <h1>Welcome, please sign up.</h1>
+        <form onSubmit={handleSubmit}>
+        <h5>Username:</h5>
+          <input
+            id='username'
+            className='form-field'
+            type='text'
+            name='username'
+            value={userData.username}
+            onChange={handleUsernameInputChange}
+          />
+          <h5>Password:</h5>
+          <input
+            id='username'
+            className='form-field'
+            type='password'
+            name='username'
+            value={userData.password}
+            onChange={handlePasswordInputChange}
+          />
+          <input type='submit' value='Submit' />
+        </form>
+        </div>
+      </div>
     </div>
   );
 };
